6368c949d6050c1ce29ea2e1dc9b9dc701397f1c
MykolaH1 <mh22051993@gmail.com>
2022-08-08 11:31:04 +0300

    перше завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,54 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+/*
+Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+Додасть назву інгредієнта як його текстовий вміст.
+Додасть елементу клас item.
+Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+*/
+
+//створюю функцію для списку
+const makelist = () => {
+  //Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+  const listEl = document.createElement('li');
+  //Додасть елементу клас item.
+  listEl.classList.add('item');
+
+
+}
+
+const test = makelist(ingredients[0]);
+console.log(test);
+
+
+
+
+
+
+
+
+
+
+//шукаю чи є ul
+//const listEl = document.querySelector('#ingredients');
+
+
+
+//Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//const list = document.createElement('li');
+
+
+//Додасть назву інгредієнта як його текстовий вміст.
+
+
+
+//Додасть елементу клас item.
+//list.classList.add('item');
+
+
+//Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+//listEl.append(list);
+
