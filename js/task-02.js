51aab371f65e66dffe92b970da7b97e9a3693347
MykolaH1 <mh22051993@gmail.com>
2022-08-08 16:55:11 +0300

    завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -9,51 +9,22 @@ const ingredients = [
 
 
 /*
-Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
-Додасть назву інгредієнта як його текстовий вміст.
-Додасть елементу клас item.
-Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+1)Створить окремий елемент <li>.
+Обов'язково використовуй метод document.createElement().
+2)Додасть назву інгредієнта як його текстовий вміст.
+3)Додасть елементу клас item.
+4)Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
 */
 
-//створюю функцію для списку
-const makelist = () => {
-  //Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
-  const listEl = document.createElement('li');
-  //Додасть елементу клас item.
-  listEl.classList.add('item');
+
+const listEl = document.querySelector('#ingredients');
 
 
-}
+const ing = ingredients.map(el => {
+  const list = document.createElement('li');
+  list.classList.add('item');
+  list.textContent = el;
+  return list;
+});
 
-const test = makelist(ingredients[0]);
-console.log(test);
-
-
-
-
-
-
-
-
-
-
-//шукаю чи є ul
-//const listEl = document.querySelector('#ingredients');
-
-
-
-//Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
-//const list = document.createElement('li');
-
-
-//Додасть назву інгредієнта як його текстовий вміст.
-
-
-
-//Додасть елементу клас item.
-//list.classList.add('item');
-
-
-//Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
-//listEl.append(list);
-
+listEl.append(...ing);
