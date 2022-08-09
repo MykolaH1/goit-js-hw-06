2cad94405c9977c4871515e7a4d2ff75b1f7c7a3
MykolaH1 <mh22051993@gmail.com>
2022-08-09 18:38:30 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const colorTitleEl = document.querySelector('.color');
+const buttonChangeColorEL = document.querySelector('.change-color');
+
+
+
+
+
+buttonChangeColorEL.addEventListener("click", () => {
+  colorTitleEl.textContent = getRandomHexColor();
+
+});
