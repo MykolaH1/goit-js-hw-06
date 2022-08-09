72d0a42173e9d4d59465674b379f6ed3f0708186
MykolaH1 <mh22051993@gmail.com>
2022-08-09 16:06:54 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,9 @@
-'use strict';
 
-
-
-
-const symbolInputEl = document.querySelector('input');
-const symbolInputDataEl = document.querySelector('input[data-length = "6"]');
+const symbolInputEl = document.querySelector('#validation-input');
+const symbolInputDataEl = document.querySelector('#validation-input[data-length="6"]');
 
 symbolInputEl.addEventListener('blur', () => {
-   if (symbolInputEl.value.length !== Number(symbolInputDataEl.dataset.length)) {
+   if (symbolInputEl.value.length <= Number(symbolInputDataEl.dataset.length)) {
       symbolInputEl.classList.remove('valid');
       symbolInputEl.classList.add('invalid');
    } else {
